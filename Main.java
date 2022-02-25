2cc16a6da879d7ec29df4420a2843e03acf969eb
Garhett <60421166+garhett@users.noreply.github.com>
2022-02-25 22:27:23 +0000

    Added test cases and method template

--- Main.java
+++ Main.java
@@ -8,8 +8,15 @@
 // s = "program", t = "function", return false
 
 class Main {
+  
+  public static boolean isValidAnagram(String s, String t) {
+    // todo finish the log to test valid anagram here. Change return value
+      return true;
+    }
+
   public static void main(String[] args) {
-    String s = "cat";
-    String t = "tac";
-  }
+    System.out.println("is valid anagram? " + isValidAnagram("cat", "tac")); // should be true
+    System.out.println("is valid anagram? " + isValidAnagram("listen", "silent")); // should be true
+    System.out.println("is valid anagram? " + isValidAnagram("program", "function")); // should be false
+   }
 }
