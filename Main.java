73e778c93b528b5ac069eae949335b4d9299b723
Garhett <60421166+garhett@users.noreply.github.com>
2022-02-26 18:28:31 +0000

    More pushes

--- Main.java
+++ Main.java
@@ -8,15 +8,15 @@
 // s = "program", t = "function", return false
 
 class Main {
-  
-  public static boolean isValidAnagram(String s, String t) {
-    // todo finish the log to test valid anagram here. Change return value
-      return true;
-    }
 
-  public static void main(String[] args) {
-    System.out.println("is valid anagram? " + isValidAnagram("cat", "tac")); // should be true
-    System.out.println("is valid anagram? " + isValidAnagram("listen", "silent")); // should be true
-    System.out.println("is valid anagram? " + isValidAnagram("program", "function")); // should be false
-   }
+	public static boolean isValidAnagram(String s, String t) {
+		// todo finish the log to test valid anagram here. Change return value
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("is valid anagram? " + isValidAnagram("cat", "tac")); // should be true
+		System.out.println("is valid anagram? " + isValidAnagram("listen", "silent")); // should be true
+		System.out.println("is valid anagram? " + isValidAnagram("program", "function")); // should be false
+	}
 }
