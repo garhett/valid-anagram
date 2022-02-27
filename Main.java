bb064a06c0289cf02255e63ed3a8047ba142f8f2
Garhett <60421166+garhett@users.noreply.github.com>
2022-02-27 04:00:32 +0000

    final submission

--- Main.java
+++ Main.java
@@ -11,17 +11,17 @@ class Main {
 
 	public static boolean isValidAnagram(String s, String t) {
 		
-		if(s == null && t == null) 
+		if (s == null && t == null) 
 			return true;
-		if((s == null || t == null) || (s.length() != t.length())) 
+		if ((s == null || t == null) || (s.length() != t.length())) 
 			return false;
         
 		int[] ct = new int[26];
         
-		for(int i = 0; i < s.length(); i++) {
+		for (int i = 0; i < s.length(); i++) {
 			ct[s.charAt(i)-'a']++;
 		}
-		for(int i = 0; i < t.length(); i++) {
+		for (int i = 0; i < t.length(); i++) {
 			ct[t.charAt(i)-'a']--;
 			if (ct[t.charAt(i)-'a'] < 0) 
 				return false;
