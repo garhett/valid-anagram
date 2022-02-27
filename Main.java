7998b4b04a4afd1cf307c86c7d64ca2f877ca530
Garhett <60421166+garhett@users.noreply.github.com>
2022-02-27 03:57:29 +0000

    Final change--submission

--- Main.java
+++ Main.java
@@ -10,13 +10,32 @@
 class Main {
 
 	public static boolean isValidAnagram(String s, String t) {
-		// todo finish the log to test valid anagram here. Change return value
+		
+		if(s==null && t == null) 
+			return true;
+		if((s==null || t == null) || (s.length() != t.length())) 
+			return false;
+        
+		int[] ct= new int[26];
+        
+		for(int i = 0; i < s.length(); i++) {
+			ct[s.charAt(i)-'a']++;
+		}
+		for(int i = 0; i < t.length(); i++) {
+			ct[t.charAt(i)-'a']--;
+			if (ct[t.charAt(i)-'a'] < 0) 
+				return false;
+		}
+		
 		return true;
 	}
+
+	// compare c in cat with all letters of tac. If c doesn't equal any letters in tac, then return false. Otherwise, go to a in cat and compare again.
 
 	public static void main(String[] args) {
 		System.out.println("is valid anagram? " + isValidAnagram("cat", "tac")); // should be true
 		System.out.println("is valid anagram? " + isValidAnagram("listen", "silent")); // should be true
 		System.out.println("is valid anagram? " + isValidAnagram("program", "function")); // should be false
+		System.out.println("is valid anagram? " + isValidAnagram("stations", "function")); // should be false
 	}
 }
